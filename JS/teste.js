452289ad0990d94784494ce7b72cfefaf0256677
adaoUFT <103225443+adaoUFT@users.noreply.github.com>
2022-04-10 21:30:53 -0300

    R11 (#9)
    * R11/INTERAÇÃO2/primeiro COMMI
    
    * R11/INTERAÇÃO2/SEGUNDO COMMIT
    
    * 'R11/INTERAÇÃO2/TERCEIRO COMMIT

--- JS/teste.js
+++ JS/teste.js
@@ -18,6 +18,12 @@ console.log('R01/INTERAÇÃO2/TERCEIRO COMMIT');
 console.log(to+te);
 
 
+
+console.log('R11/INTERAÇÃO2/primeiro COMMIT');
+console.log('R11/INTERAÇÃO2/SEGUNDO COMMIT');
+console.log('R11/INTERAÇÃO2/TERCEIRO COMMIT');
+
 console.log('R10/INTERAÇÃO2/primeiro  COMMIT');
 console.log('R01/INTERAÇÃO2/segundo COMMIT');
 console.log('R01/INTERAÇÃO2/terceiro COMMIT');
+
